8602bf0763f9944ca36b0b186e04db69182c2f89
elenkalee <elena.lipukhina@gmail.com>
2021-02-26 17:29:57 +0300

    updated pom.xml and StatisticsServiceTest.java

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -22,12 +22,11 @@ class StatisticsServiceTest {
   void findMaxFromMinSales() {
     StatisticsService service = new StatisticsService();
 
-    long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 1};
+    long[] incomesInBillions = {1, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
     long expected = 12;
 
     long actual = service.findMax(incomesInBillions);
 
     assertEquals(expected, actual);
-
   }
 }
